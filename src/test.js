b4e98529ababe7ff894cfc6370af4f4be087fc8f
zhouxiaoyi021 <zhouxiaoyi021@163.com>
2017-11-25 15:53:59 +0800

    修改了src/test.js

--- src/test.js
+++ src/test.js
@@ -1,3 +1,6 @@
 let div = document.createElement('div')
 div.innerHTML = 'hello webapck'
 document.body.appendChild(div)
+
+console.log('hello webpack')
+
--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,4 @@ module.exports = {
     filename:'bundle.js'
   }
 }
+
